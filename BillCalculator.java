be6f2666d24f0c96ca5d8bee0d665cb869ab8f9c
bhumika kothari <bhumika.25bai10951@vitbhopal.ac.in>
2026-09-16 22:25:57 +0530

    Update BillCalculator.java

--- BillCalculator.java
+++ BillCalculator.java
@@ -1,28 +1,22 @@
 public class BillCalculator {
-
-    // Calculate electricity bill according to units
     public static double calculateBill(int units) {
-
         double bill = 0;
 
         // First 100 units
         if (units <= 100) {
             bill = units * 3.0;
         }
-
         // 101 to 200 units
         else if (units <= 200) {
             bill = (100 * 3.0)
                     + ((units - 100) * 5.0);
         }
-
         // 201 to 300 units
         else if (units <= 300) {
             bill = (100 * 3.0)
                     + (100 * 5.0)
                     + ((units - 200) * 7.0);
         }
-
         // More than 300 units
         else {
             bill = (100 * 3.0)
@@ -30,7 +24,6 @@ public class BillCalculator {
                     + (100 * 7.0)
                     + ((units - 300) * 10.0);
         }
-
         return bill;
     }
 }
