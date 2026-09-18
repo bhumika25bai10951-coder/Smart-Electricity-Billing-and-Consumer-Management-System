7c9fac4214a3cd889986fb2dfaad5f6e07882e91
bhumika kothari <bhumika.25bai10951@vitbhopal.ac.in>
2026-09-18 19:56:17 +0530

    Simplify bill calculation logic
    Refactor bill calculation logic for clarity.

--- BillCalculator.java
+++ BillCalculator.java
@@ -8,21 +8,15 @@ public class BillCalculator {
         }
         // 101 to 200 units
         else if (units <= 200) {
-            bill = (100 * 3.0)
-                    + ((units - 100) * 5.0);
+            bill = (100 * 3.0) + ((units - 100) * 5.0);
         }
         // 201 to 300 units
         else if (units <= 300) {
-            bill = (100 * 3.0)
-                    + (100 * 5.0)
-                    + ((units - 200) * 7.0);
+            bill = (100 * 3.0) + (100 * 5.0) + ((units - 200) * 7.0);
         }
         // More than 300 units
         else {
-            bill = (100 * 3.0)
-                    + (100 * 5.0)
-                    + (100 * 7.0)
-                    + ((units - 300) * 10.0);
+            bill = (100 * 3.0) + (100 * 5.0)   + (100 * 7.0) + ((units - 300) * 10.0);
         }
         return bill;
     }
