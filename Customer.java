5534d8e4924d8accccc339ba08c3292f1702a1b1
bhumika kothari <bhumika.25bai10951@vitbhopal.ac.in>
2026-09-18 19:57:04 +0530

    Fix formatting in Customer.java constructor and method

--- Customer.java
+++ Customer.java
@@ -5,14 +5,14 @@ public class Customer {
     String meterNumber;
     String address;
     
-    public Customer(int customerId, String name, String meterNumber, String address) {
+public Customer(int customerId, String name, String meterNumber, String address) {
         this.customerId = customerId;
         this.name = name;
         this.meterNumber = meterNumber;
         this.address = address;
     }
 
-    public void displayCustomer() {
+public void displayCustomer() {
         System.out.println("Customer ID       : " + customerId);
         System.out.println("Customer Name     : " + name);
         System.out.println("Meter Number      : " + meterNumber);
